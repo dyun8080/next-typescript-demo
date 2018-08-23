400a3f9bc965900e82fc8458a5426aa64d6ffd91
dyun8080 <dyun8080@163.com>
2018-08-23 18:59:47 +0800

    ♻️ update

--- production-server/index.js
+++ production-server/index.js
@@ -1,23 +1,41 @@
 "use strict";
+/**
+ * @see https://github.com/zeit/next.js/tree/canary/examples/custom-server-koa
+ * @see https://github.com/zeit/next.js/tree/canary/examples/custom-server-typescript
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
+const Koa = require("koa");
 const next = require("next");
+const compression = require("compression");
+const koaConnect = require("koa-connect");
+const Router = require("koa-router");
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 app.prepare()
     .then(() => {
-    const server = express();
-    server.get('/a', (req, res) => {
-        return app.render(req, res, '/a', req.query);
+    const server = new Koa();
+    const router = new Router();
+    router.get('/a', async (ctx) => {
+        await app.render(ctx.req, ctx.res, '/b', ctx.query);
+        ctx.respond = false;
     });
-    server.get('*', (req, res) => {
-        return handle(req, res);
+    router.get('/b', async (ctx) => {
+        await app.render(ctx.req, ctx.res, '/a', ctx.query);
+        ctx.respond = false;
     });
-    server.listen(port, (err) => {
-        if (err)
-            throw err;
+    router.get('*', async (ctx) => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+    });
+    server.use(koaConnect(compression()));
+    server.use(async (ctx, next) => {
+        ctx.res.statusCode = 200;
+        await next();
+    });
+    server.use(router.routes());
+    server.listen(port, () => {
         console.log(`> Ready on http://localhost:${port}`);
     });
 });
